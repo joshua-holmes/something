95f3b9da4f364ec131b4170862181b905516063b
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-05-10 20:59:58 -0700

    Improve help message

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -2,9 +2,11 @@ use std::fmt::Display;
 
 pub fn print_help(prefix: impl Display) {
     println!("{}
-something <FILE_NAME>
 
-something -h | --help
+something <FILE_NAME>
+    executes the specified 'something' script
+
+something [-h | --help]
     prints this help message", prefix);
 }
 
